24c760e98715318a1484b44c39a8e46a2208d272
yosuf.haydary@gmail.com <yosuf.haydary@gmail.com@819e53d7-5d2a-dd2e-245e-3269015b4709>
2013-06-24 17:26:26 +0000

    tabular ordering for strings to line them up in a column

--- useful/src/main/java/julius/utilities/StringHelper.java
+++ useful/src/main/java/julius/utilities/StringHelper.java
@@ -16,6 +16,8 @@
 
 package julius.utilities;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
@@ -24,19 +26,22 @@ import julius.reflection.ToStringTask;
 import julius.validation.Assertions;
 
 /**
- * provides methods to parse text (to easily parse valid.invalid html for example)
- * or flatten a List<Object> to String
+ * provides methods to parse text (to easily parse valid.invalid html for example) or flatten a
+ * List<Object> to String
+ * 
+ * @author AJG van Schie
+ * @author Yosuf Haydary
  */
 public final class StringHelper {
 
-	private StringHelper(){
+	private StringHelper() {
 		super();
 	}
-	
+
 	// some string constants which are used a lot (some are also on StringUtils)
 	/** empty = ''; */
 	public static final String EMPTY = "";
-	/** single whitespace ' '*/
+	/** single whitespace ' ' */
 	public static final String SPACE = " ";
 	/** 'J' */
 	public static final String J = "J";
@@ -44,30 +49,32 @@ public final class StringHelper {
 	public static final String Y = "Y";
 	/** 'N' */
 	public static final String N = "N";
-	
+
 	/**
 	 * puts all items of the list separated by separator in a string
 	 * 
 	 * alternatively use apache commons
-	 * @param list null/empty safe
+	 * 
+	 * @param list
+	 *            null/empty safe
 	 * @param separator
 	 * @return
 	 */
-	public static <T> String flatten(final List<T> list, final String separator){
-		if(CollectionHelper.isEmpty(list)){
+	public static <T> String flatten(final List<T> list, final String separator) {
+		if (CollectionHelper.isEmpty(list)) {
 			return EMPTY;
 		}
 		Object last = CollectionHelper.last(list);
 		StringBuilder sb = new StringBuilder();
 		for (Object o : CollectionHelper.allExceptLast(list)) {
-				sb.append(o.toString());
-				sb.append(separator);
+			sb.append(o.toString());
+			sb.append(separator);
 		}
 		sb.append(last.toString());
 		return sb.toString();
 	}
-	
-	/**
+
+/**
 	 * takes the substring between the Nth occurrence of startExpression and the next endExpression
 	 * 
 	 * example
@@ -84,61 +91,181 @@ public final class StringHelper {
 	 * @param nrOfOccurence
 	 * @return
 	 */
-	public static String parse(final String wholeString, final String startExpression, final String endExpression, final int nrOfOccurence) {
-		int startExpPosition = getPositionOfNthOccurence(wholeString,startExpression,nrOfOccurence);
-		Assertions.argument.assertTrue(startExpPosition >=0, "start expression ("+startExpression+") could not be found N ("+nrOfOccurence+") times in the string");
-		int substringStart = startExpPosition+startExpression.length();  
-		int substringEnd = wholeString.indexOf(endExpression, substringStart);// start searching for end after start expression
+	public static String parse(final String wholeString, final String startExpression,
+			final String endExpression, final int nrOfOccurence) {
+		int startExpPosition = getPositionOfNthOccurence(wholeString, startExpression,
+				nrOfOccurence);
+		Assertions.argument.assertTrue(startExpPosition >= 0, "start expression ("
+				+ startExpression + ") could not be found N (" + nrOfOccurence
+				+ ") times in the string");
+		int substringStart = startExpPosition + startExpression.length();
+		int substringEnd = wholeString.indexOf(endExpression, substringStart);// start
+																				// searching
+																				// for
+																				// end
+																				// after
+																				// start
+																				// expression
 		return wholeString.substring(substringStart, substringEnd);
 	}
 
 	/**
 	 * finds the Nth occurrence of the expression in the wholeString and returns its position or -1
+	 * 
 	 * @param wholeString
 	 * @param expression
 	 * @param nrOfOccurence
 	 * @return position or -1
 	 */
-	public static int getPositionOfNthOccurence(final String wholeString, final String expression, final int nrOfOccurence){
-		Assertions.argument.assertTrue(nrOfOccurence >0, "nr of occurences must be > 0");
+	public static int getPositionOfNthOccurence(final String wholeString, final String expression,
+			final int nrOfOccurence) {
+		Assertions.argument.assertTrue(nrOfOccurence > 0, "nr of occurences must be > 0");
 		Assertions.argument.assertNotNull(wholeString, "the string to be searched");
 		Assertions.argument.assertNotNull(expression, "the expression to be searched for");
-		
+
 		int pos = 0;
-		for(int i=0;i<nrOfOccurence && pos>=0 ;i++){
-			pos = wholeString.indexOf(expression,pos+1);
+		for (int i = 0; i < nrOfOccurence && pos >= 0; i++) {
+			pos = wholeString.indexOf(expression, pos + 1);
 		}
 		return pos;
 	}
-	
+
 	/**
-	 * recursively traverse getters and collections to create a string for root
-	 * supports multiple object occurrence(only print once)
+	 * recursively traverse getters and collections to create a string for root supports multiple
+	 * object occurrence(only print once)
+	 * 
 	 * @param root
 	 * @return
 	 */
-	public static String toStringRecursive(final Object root){
+	public static String toStringRecursive(final Object root) {
 		ToStringTask task = new ToStringTask();
 		BeanTraverser.traverseRecursive(task, root);
 		return task.toString();
 	}
-	
+
 	/**
 	 * 
-	 * recursively traverse getters and collections to create a string for root
-	 * supports multiple object occurrence(only print once)
+	 * recursively traverse getters and collections to create a string for root supports multiple
+	 * object occurrence(only print once)
+	 * 
 	 * @param root
-	 * @param maxDepth prevent to large trees
+	 * @param maxDepth
+	 *            prevent to large trees
 	 * @param skipPaths
 	 * @param skipMethods
 	 * @param skipClasses
 	 * @return
 	 */
-	public static String toStringRecursive(final Object root, final int maxDepth, final Set<String> skipPaths, final Set<String> skipMethods, final Set<String> skipClasses){
+	public static String toStringRecursive(final Object root, final int maxDepth,
+			final Set<String> skipPaths, final Set<String> skipMethods,
+			final Set<String> skipClasses) {
 		ToStringTask task = new ToStringTask(maxDepth, skipPaths, skipMethods, skipClasses);
 		BeanTraverser.traverseRecursive(task, root);
 		return task.toString();
 	}
-	
-	
+
+	/**
+	 * A useful method to format the lines to line up neatly the columns which are separated by a
+	 * tab for example. Example:
+	 * 
+	 * Given : ["4444\t333\t22" , "1\t22\t4444" ] with "\t" as splitter
+	 * 
+	 * Return: ["4444\t333\t22  " , "1   \t22 \t4444" ]
+	 * 
+	 * Which can be nicely printed
+	 * 
+	 * @param lines
+	 *            (not null)
+	 * @param splitter
+	 *            (not null) regex
+	 * @return
+	 */
+	public static List<String> formatLines(final Collection<String> lines, final String splitter) {
+		Assertions.argument.assertNotNull(lines, "lines");
+		Assertions.argument.assertNotNull(splitter, "splitter");
+
+		List<Integer> maxIndexLengths = determineMaxIndexLengths(lines, splitter);
+		List<String> allLinesFormatted = CollectionHelper.list();
+
+		for (String line : lines) {
+			StringBuilder lineFormatted = new StringBuilder();
+
+			String[] lineSplitted = line.split(splitter);
+
+			for (int index = 0; index < lineSplitted.length; index++) {
+				String indexVal = lineSplitted[index];
+
+				lineFormatted.append(indexVal).append(
+						createWhiteSpace(maxIndexLengths.get(index) - indexVal.length()));
+
+				if (index < lineSplitted.length - 1) {
+					lineFormatted.append(splitter);
+				}
+
+			}
+			allLinesFormatted.add(lineFormatted.toString());
+		}
+
+		return allLinesFormatted;
+	}
+
+	/**
+	 * For the list of given lines this method determines what the maximum length of characters per
+	 * split of each index can be.
+	 * 
+	 * Example: line 1: "abc,a,da" line 2: "a,bc" splitChar: ","
+	 * 
+	 * Result: [3,2,2] because the zeroth index of first line has the max chars; the first index of
+	 * second line has max characters; and only the first line has second index
+	 * 
+	 * @param lines
+	 *            (not null)
+	 * @param splitter
+	 *            (not null) Regex: The character(s) which is used to split the lines by
+	 * @return (not null), but maybe empty
+	 */
+	protected static List<Integer> determineMaxIndexLengths(final Collection<String> lines,
+			final String splitter) {
+
+		List<Integer> maxCellLengthsPerColumn = new ArrayList<Integer>();
+
+		for (String line : lines) {
+
+			String[] lineSplitted = line.split(splitter);
+
+			for (int index = 0; index < lineSplitted.length; index++) {
+
+				Integer existingCellLenght = null;
+				// check whether this index exists in the list yet
+				if (index < maxCellLengthsPerColumn.size()) {
+					existingCellLenght = maxCellLengthsPerColumn.get(index);
+				}
+
+				int currentIndexLength = lineSplitted[index].length();
+
+				if (existingCellLenght == null) {
+					maxCellLengthsPerColumn.add(index, currentIndexLength);
+				} else if (currentIndexLength > existingCellLenght) {
+					maxCellLengthsPerColumn.set(index, currentIndexLength);
+				}
+			}
+		}
+
+		return maxCellLengthsPerColumn;
+	}
+
+	/**
+	 * Creates a string existing out of white spaces for the given count.
+	 * 
+	 * @param count
+	 * @return (not null), but maybe empty
+	 */
+	protected static String createWhiteSpace(final int count) {
+		String result = EMPTY;
+
+		for (int i = 0; i < count; i++) {
+			result += SPACE;
+		}
+		return result;
+	}
 }
--- useful/src/test/java/julius/utilities/TestStringHelper.java
+++ useful/src/test/java/julius/utilities/TestStringHelper.java
@@ -16,6 +16,8 @@
 
 package julius.utilities;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -29,104 +31,135 @@ import nl.testclasses.MultiParent;
 import nl.testclasses.MultiParentList;
 import nl.testclasses.Parent;
 
-public class TestStringHelper extends BDDTestCase{
+public class TestStringHelper extends BDDTestCase {
 
-	public void testParse(){
-		String whole="<html><body><p>something</p><p>important</p></body></html>";
+	private static final String SPLIT_CHAR = "\t";
 
-		given("the text:"+whole);
-			String s="<p>";
-			String e="</p";
-			int nr = 1;
-		when("queried for text between the "+nr+"th occurence of "+s+" and the next "+e);
-			
-			String result = StringHelper.parse(whole, s, e, nr);		
-			String expected = "something"; 
-			
-		then("we expect the result:"+expected);
-			
-			assertEquals(expected, result);
-			
-			nr = 2;
-		when("queried for text between the "+nr+"th occurence of "+s+" and the next "+e);
-			
-			result = StringHelper.parse(whole, s, e, nr);		
-			expected = "important"; 
-			
-		then("we expect the result:"+expected);
-			assertEquals(expected, result);
-			
-			s="body>";
-			e="</p";
-			nr = 1;
-		when("queried for text between the "+nr+"th occurence of "+s+" and the next "+e);
-			result = StringHelper.parse(whole, s, e, nr);		
-			expected = "<p>something"; 
-			
-		then("we expect the result:"+expected);
-			assertEquals(expected, result);
+	public void testDetermineMaxIndexLengths() {
+		// empty line should return empty
+		assertTrue(StringHelper.determineMaxIndexLengths(new ArrayList<String>(), "").isEmpty());
 
-			nr = 10000;
-		when("queried for N("+nr+") larger then actual occurences");
-		
+		String line1 = "88888888" + SPLIT_CHAR + "4444" + SPLIT_CHAR + "22";
+		String line2 = "333" + SPLIT_CHAR + "55555";
+		String line3 = "1" + SPLIT_CHAR + "7777777" + SPLIT_CHAR + "1" + SPLIT_CHAR + "333";
+
+		List<String> lines = CollectionHelper.list(line1, line2, line3);
+
+		List<Integer> expected = CollectionHelper.list(8, 7, 2, 3);
+
+		List<Integer> result = StringHelper.determineMaxIndexLengths(lines, SPLIT_CHAR);
+		assertEquals(expected, result);
+
+		expected = CollectionHelper.list(16);
+		// now we expect the max length of the longest line because the split char does not occur
+		assertEquals(expected, StringHelper.determineMaxIndexLengths(lines, ","));
+
+		Collection<String> formatted = StringHelper.formatLines(lines, SPLIT_CHAR);
+
+		for (String item : formatted) {
+			System.out.println(item);
+		}
+	}
+
+	public void testCreateWhiteSpace() {
+		assertEquals("", StringHelper.createWhiteSpace(0));
+		assertEquals(" ", StringHelper.createWhiteSpace(1));
+		assertEquals("     ", StringHelper.createWhiteSpace(5));
+	}
+
+	public void testParse() {
+		String whole = "<html><body><p>something</p><p>important</p></body></html>";
+
+		given("the text:" + whole);
+		String s = "<p>";
+		String e = "</p";
+		int nr = 1;
+		when("queried for text between the " + nr + "th occurence of " + s + " and the next " + e);
+
+		String result = StringHelper.parse(whole, s, e, nr);
+		String expected = "something";
+
+		then("we expect the result:" + expected);
+
+		assertEquals(expected, result);
+
+		nr = 2;
+		when("queried for text between the " + nr + "th occurence of " + s + " and the next " + e);
+
+		result = StringHelper.parse(whole, s, e, nr);
+		expected = "important";
+
+		then("we expect the result:" + expected);
+		assertEquals(expected, result);
+
+		s = "body>";
+		e = "</p";
+		nr = 1;
+		when("queried for text between the " + nr + "th occurence of " + s + " and the next " + e);
+		result = StringHelper.parse(whole, s, e, nr);
+		expected = "<p>something";
+
+		then("we expect the result:" + expected);
+		assertEquals(expected, result);
+
+		nr = 10000;
+		when("queried for N(" + nr + ") larger then actual occurences");
+
 		then("we should get an exception");
-			try{
-				result = StringHelper.parse(whole, s, e, nr);						
-				fail("exception expected not:"+result);
-			}catch(IllegalArgumentException easd){}
-			
-		
+		try {
+			result = StringHelper.parse(whole, s, e, nr);
+			fail("exception expected not:" + result);
+		} catch (IllegalArgumentException easd) {
+		}
+
 		successFullStory();
 	}
-	
-	public void testGetNposition(){
+
+	public void testGetNposition() {
 		String w = "<a>asd</a> <b>ad</b>";
 		note("non existing string should return -1 position");
-		assertEquals(-1, StringHelper.getPositionOfNthOccurence(w, "asdasdasd",1));
+		assertEquals(-1, StringHelper.getPositionOfNthOccurence(w, "asdasdasd", 1));
 		note("existing string but not enough times, should return -1 position");
-		assertEquals(-1, StringHelper.getPositionOfNthOccurence(w, "a",1000));
+		assertEquals(-1, StringHelper.getPositionOfNthOccurence(w, "a", 1000));
 		note("normal first occurence");
-		assertEquals(1, StringHelper.getPositionOfNthOccurence(w, "a",1));
+		assertEquals(1, StringHelper.getPositionOfNthOccurence(w, "a", 1));
 		note("normal n occurence");
-		assertEquals(8, StringHelper.getPositionOfNthOccurence(w, "a",3));	
+		assertEquals(8, StringHelper.getPositionOfNthOccurence(w, "a", 3));
 	}
-	
-	public void testFlatten(){
+
+	public void testFlatten() {
 		String separator = ";";
 		List<Integer> lNull = null;
-		note("flatten "+lNull+" should be empty ''");
+		note("flatten " + lNull + " should be empty ''");
 		assertEquals(StringHelper.EMPTY, StringHelper.flatten(lNull, separator));
-		
+
 		List<Integer> l = CollectionHelper.list();
-		note("flatten "+l+" should be empty ''");
+		note("flatten " + l + " should be empty ''");
 		assertEquals(StringHelper.EMPTY, StringHelper.flatten(l, separator));
-		
+
 		List<Integer> l1 = CollectionHelper.list(1);
-		note("flatten "+l1+" item should be '1'");
+		note("flatten " + l1 + " item should be '1'");
 		assertEquals("1", StringHelper.flatten(l1, separator));
-		
-		List<Integer> l13 = CollectionHelper.list(1,3);
-		note("flatten "+l13+" item should be '1;3'");
+
+		List<Integer> l13 = CollectionHelper.list(1, 3);
+		note("flatten " + l13 + " item should be '1;3'");
 		assertEquals("1;3", StringHelper.flatten(l13, separator));
-		
-		
-		List<Integer> l13456 = CollectionHelper.list(1,3,4,5,6);
-		note("flatten "+l13456+" item should be '1;3'");
+
+		List<Integer> l13456 = CollectionHelper.list(1, 3, 4, 5, 6);
+		note("flatten " + l13456 + " item should be '1;3'");
 		assertEquals("1;3;4;5;6", StringHelper.flatten(l13456, separator));
-		
+
 	}
-	
-	
-	public void testPrintCyclic(){
+
+	public void testPrintCyclic() {
 		Cycle c1 = new Cycle(1);
 		c1.setCycle(c1);
-		Cycle c2 =new Cycle(2);
+		Cycle c2 = new Cycle(2);
 		c2.setCycle(c1);
 
-//		System.out.println(StringHelper.toStringRecursive(c1));
-//		System.out.println(StringHelper.toStringRecursive(c2));
-		
-		
+		// System.out.println(StringHelper.toStringRecursive(c1));
+		// System.out.println(StringHelper.toStringRecursive(c2));
+
 		Cycle c3 = new Cycle(3);
 		Cycle c4 = new Cycle(4);
 		Cycle c5 = new Cycle(5);
@@ -134,151 +167,155 @@ public class TestStringHelper extends BDDTestCase{
 		c4.setCycle(c5);
 		c5.setCycle(c2);
 		c1.setCycle(c3);
-//		System.out.println(StringHelper.toStringRecursive(c1));
-//		System.out.println(StringHelper.toStringRecursive(c2));
-//		System.out.println(StringHelper.toStringRecursive(c3));
-//		System.out.println(StringHelper.toStringRecursive(c4));
+		// System.out.println(StringHelper.toStringRecursive(c1));
+		// System.out.println(StringHelper.toStringRecursive(c2));
+		// System.out.println(StringHelper.toStringRecursive(c3));
+		// System.out.println(StringHelper.toStringRecursive(c4));
 		System.out.println(StringHelper.toStringRecursive(c5));
-		String res =StringHelper.toStringRecursive(c5);
-		assertTrue(containsOnce(res,"getI: 1"));
-		assertTrue(containsOnce(res,"getI: 2"));
-		assertTrue(containsOnce(res,"getI: 3"));
-		assertTrue(containsOnce(res,"getI: 4"));
-		assertTrue(containsOnce(res,"getI: 5"));
+		String res = StringHelper.toStringRecursive(c5);
+		assertTrue(containsOnce(res, "getI: 1"));
+		assertTrue(containsOnce(res, "getI: 2"));
+		assertTrue(containsOnce(res, "getI: 3"));
+		assertTrue(containsOnce(res, "getI: 4"));
+		assertTrue(containsOnce(res, "getI: 5"));
 	}
-	
-	public boolean containsOnce(final String res, final String part){
-		return res.indexOf(part) >-1 && res.indexOf(part, res.indexOf(part)+1)==-1;
+
+	public boolean containsOnce(final String res, final String part) {
+		return res.indexOf(part) > -1 && res.indexOf(part, res.indexOf(part) + 1) == -1;
 	}
-	
-	
-    public void testPrintRecursive() {
-        note("should support partial filled object");
 
-        Child c = new Child();
-        c.setVal(1);
-        c.setVal2(2);
-        String res = StringHelper.toStringRecursive(c);
+	public void testPrintRecursive() {
+		note("should support partial filled object");
 
-        assertTrue(containsOnce(res,"Child"));
-        assertTrue(containsOnce(res,"getVal: 1"));
-        assertTrue(containsOnce(res,"getVal2: 2"));
+		Child c = new Child();
+		c.setVal(1);
+		c.setVal2(2);
+		String res = StringHelper.toStringRecursive(c);
 
-        c.setVal(null);
-        c.setVal2(null);
-        
-        res = StringHelper.toStringRecursive(c);
+		assertTrue(containsOnce(res, "Child"));
+		assertTrue(containsOnce(res, "getVal: 1"));
+		assertTrue(containsOnce(res, "getVal2: 2"));
 
-        assertTrue(containsOnce(res,"Child"));
-        assertTrue(containsOnce(res,"getVal: null"));
-        
-        note("a fully filled object with filled sub object should print");
+		c.setVal(null);
+		c.setVal2(null);
 
-        Parent p = new Parent();
-        p.setVal(1);
-        p.setC(c);
-        
-        res = StringHelper.toStringRecursive(p);
-        System.out.println(res);
-        assertTrue(containsOnce(res,"Parent"));
-        assertTrue(containsOnce(res,"getC: "));
-        assertTrue(containsOnce(res,"getVal: null"));
-        
-        res = StringHelper.toStringRecursive(p,1, new HashSet<String>(), new HashSet<String>(), new HashSet<String>());
-        System.out.println(res);
+		res = StringHelper.toStringRecursive(c);
 
-        assertTrue(containsOnce(res,"Parent"));
-        assertTrue(containsOnce(res,"getC: "));
-        assertFalse(containsOnce(res,"getVal: null"));
+		assertTrue(containsOnce(res, "Child"));
+		assertTrue(containsOnce(res, "getVal: null"));
 
-       note("a (multi) parent class with empty list of children should print");
+		note("a fully filled object with filled sub object should print");
 
-        MultiParent mp = new MultiParent();
-        mp.setVal(1);
-        mp.setC(new HashSet<Child>());
-        
-        res = StringHelper.toStringRecursive(mp);
-        System.out.println(res);
-        assertTrue(containsOnce(res,"MultiParent"));
-        assertTrue(containsOnce(res,"getC: []"));
-        assertTrue(containsOnce(res,"getVal: 1"));
-        
-        note("a (multi) parent class with list of filled children should print");
-        mp.setC(new HashSet<Child>());
-        c.setVal(1);
-        c.setVal2(2);
-        mp.getC().add(c);
+		Parent p = new Parent();
+		p.setVal(1);
+		p.setC(c);
 
-        res = StringHelper.toStringRecursive(mp);
-        System.out.println(res);
-        assertTrue(containsOnce(res,"MultiParent"));
-        assertTrue(containsOnce(res,"getC: [nl."));
+		res = StringHelper.toStringRecursive(p);
+		System.out.println(res);
+		assertTrue(containsOnce(res, "Parent"));
+		assertTrue(containsOnce(res, "getC: "));
+		assertTrue(containsOnce(res, "getVal: null"));
 
-        note("a (multi) parent class with list of filled children and null should print");
-        
-        mp.getC().add(null);
-        Child c2 = new Child();
-        c2.setVal(3);
-        mp.getC().add(c2);
-        
-        res = StringHelper.toStringRecursive(mp);
-        System.out.println(res);
-        assertTrue(containsOnce(res,"MultiParent"));
-        assertTrue(containsOnce(res,"getC: [n"));
-        assertTrue(containsOnce(res,"getVal2: 2"));
+		res = StringHelper.toStringRecursive(p, 1, new HashSet<String>(), new HashSet<String>(),
+				new HashSet<String>());
+		System.out.println(res);
 
-        note("a (multi) parent class with list of filled children [DOUBLE] and null should print");
-        MultiParentList mpl = new MultiParentList();
-        mpl.setVal(1123);
-        mpl.setC(new LinkedList<Child>());
-        mpl.getC().add(c);
-        mpl.getC().add(c2);
-        mpl.getC().add(null);
-        mpl.getC().add(c2);
-        
-        res = StringHelper.toStringRecursive(mpl);
-        System.out.println(res);
-        assertTrue(containsOnce(res,"MultiParentList"));
-        assertTrue(containsOnce(res,"skip"));
-        
-        note("support surpress exceptions");
-        ExceptionBean b = new ExceptionBean(1, "a");
-        res = StringHelper.toStringRecursive(b);
-        System.out.println(res);
-        assertTrue(containsOnce(res,"ExceptionBean"));
-        assertTrue(containsOnce(res,"FakeBeanMethod: threw"));
-        
-        note("support skip types");
-        Parent parent = new Parent();
-        parent.setVal(10);
-        parent.setC(new Child(8,9));
-        
-        res = StringHelper.toStringRecursive(parent, 1024, new HashSet<String>(), new HashSet<String>(), CollectionHelper.set("Child"));
-        System.out.println(res);
-        assertTrue(containsOnce(res,"Parent"));
-        assertTrue(containsOnce(res,"getVal: 10"));
-        assertFalse(containsOnce(res,"getVal: 8"));
-        
-        note("support skip methods");
-        
-        res = StringHelper.toStringRecursive(parent, 1024, new HashSet<String>(), CollectionHelper.set("getC"), new HashSet<String>());
-        System.out.println(res);
-        assertTrue(containsOnce(res,"Parent"));
-        assertTrue(containsOnce(res,"getVal: 10"));
-        assertFalse(containsOnce(res,"getVal: 8"));
-        
-        note("support skip methods on certain types");
-        GrandParent gp = new GrandParent();
-        gp.setC(parent);
-        
-        res = StringHelper.toStringRecursive(gp, 1024, CollectionHelper.set("nl.testclasses.Parent.getC"), new HashSet<String>(), new HashSet<String>());
-        System.out.println(res);
-        assertTrue(containsOnce(res,"nl.testclasses.GrandParent"));
-        assertTrue(containsOnce(res,"nl.testclasses.Parent")); // tostring and members
-        assertTrue(containsOnce(res,"getVal: 10"));
-        assertTrue(containsOnce(res,"nl.testclasses.Child")); // only toString not members
-        assertFalse(containsOnce(res,"getVal: 8"));
-        
-    }
+		assertTrue(containsOnce(res, "Parent"));
+		assertTrue(containsOnce(res, "getC: "));
+		assertFalse(containsOnce(res, "getVal: null"));
+
+		note("a (multi) parent class with empty list of children should print");
+
+		MultiParent mp = new MultiParent();
+		mp.setVal(1);
+		mp.setC(new HashSet<Child>());
+
+		res = StringHelper.toStringRecursive(mp);
+		System.out.println(res);
+		assertTrue(containsOnce(res, "MultiParent"));
+		assertTrue(containsOnce(res, "getC: []"));
+		assertTrue(containsOnce(res, "getVal: 1"));
+
+		note("a (multi) parent class with list of filled children should print");
+		mp.setC(new HashSet<Child>());
+		c.setVal(1);
+		c.setVal2(2);
+		mp.getC().add(c);
+
+		res = StringHelper.toStringRecursive(mp);
+		System.out.println(res);
+		assertTrue(containsOnce(res, "MultiParent"));
+		assertTrue(containsOnce(res, "getC: [nl."));
+
+		note("a (multi) parent class with list of filled children and null should print");
+
+		mp.getC().add(null);
+		Child c2 = new Child();
+		c2.setVal(3);
+		mp.getC().add(c2);
+
+		res = StringHelper.toStringRecursive(mp);
+		System.out.println(res);
+		assertTrue(containsOnce(res, "MultiParent"));
+		assertTrue(containsOnce(res, "getC: [n"));
+		assertTrue(containsOnce(res, "getVal2: 2"));
+
+		note("a (multi) parent class with list of filled children [DOUBLE] and null should print");
+		MultiParentList mpl = new MultiParentList();
+		mpl.setVal(1123);
+		mpl.setC(new LinkedList<Child>());
+		mpl.getC().add(c);
+		mpl.getC().add(c2);
+		mpl.getC().add(null);
+		mpl.getC().add(c2);
+
+		res = StringHelper.toStringRecursive(mpl);
+		System.out.println(res);
+		assertTrue(containsOnce(res, "MultiParentList"));
+		assertTrue(containsOnce(res, "skip"));
+
+		note("support surpress exceptions");
+		ExceptionBean b = new ExceptionBean(1, "a");
+		res = StringHelper.toStringRecursive(b);
+		System.out.println(res);
+		assertTrue(containsOnce(res, "ExceptionBean"));
+		assertTrue(containsOnce(res, "FakeBeanMethod: threw"));
+
+		note("support skip types");
+		Parent parent = new Parent();
+		parent.setVal(10);
+		parent.setC(new Child(8, 9));
+
+		res = StringHelper.toStringRecursive(parent, 1024, new HashSet<String>(),
+				new HashSet<String>(), CollectionHelper.set("Child"));
+		System.out.println(res);
+		assertTrue(containsOnce(res, "Parent"));
+		assertTrue(containsOnce(res, "getVal: 10"));
+		assertFalse(containsOnce(res, "getVal: 8"));
+
+		note("support skip methods");
+
+		res = StringHelper.toStringRecursive(parent, 1024, new HashSet<String>(),
+				CollectionHelper.set("getC"), new HashSet<String>());
+		System.out.println(res);
+		assertTrue(containsOnce(res, "Parent"));
+		assertTrue(containsOnce(res, "getVal: 10"));
+		assertFalse(containsOnce(res, "getVal: 8"));
+
+		note("support skip methods on certain types");
+		GrandParent gp = new GrandParent();
+		gp.setC(parent);
+
+		res = StringHelper.toStringRecursive(gp, 1024,
+				CollectionHelper.set("nl.testclasses.Parent.getC"), new HashSet<String>(),
+				new HashSet<String>());
+		System.out.println(res);
+		assertTrue(containsOnce(res, "nl.testclasses.GrandParent"));
+		assertTrue(containsOnce(res, "nl.testclasses.Parent")); // tostring and members
+		assertTrue(containsOnce(res, "getVal: 10"));
+		assertTrue(containsOnce(res, "nl.testclasses.Child")); // only toString not members
+		assertFalse(containsOnce(res, "getVal: 8"));
+
+	}
 
 }
